e9bd262d5add69e124d1a830f8b84d3c68c42425
Steve Tu <stevetu11@yahoo.com>
2020-11-12 18:36:33 -0800

    Updated debug driver.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import debug
 
 def main():
-    myDebug = debug.Debug(0.5, 0.9)
-    myDebug.testFindQuestButton()
-    myDebug.testFindRaidButton()
+    myDebug = debug.Debug(0.3, 0.9, True)
+    myDebug.testCombatMode()
 
 if __name__ == "__main__":
     main()
