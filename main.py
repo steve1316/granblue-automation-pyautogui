04389551f09724b66e53288c1308c80c199c2d54
Steve Tu <stevetu11@yahoo.com>
2020-12-19 16:48:38 -0800

    Updated main to reflect new tests.

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from debug import Debug
 def main():
     my_game = Game(custom_mouse_speed=0.3, debug_mode=True)
     my_debug = Debug(my_game)
-    my_debug.test_combat_mode()
+
+    # Test finding all summon element tabs in Summon Selection Screen.
+    # my_debug.test_find_summon_element_tabs()
+
+    # Test Combat Mode.
+    # my_debug.test_combat_mode()
 
 
 if __name__ == "__main__":
