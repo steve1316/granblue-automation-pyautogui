951e73052483ffa757f0216317935157b934712d
Steve Tu <stevetu11@yahoo.com>
2020-12-20 15:39:19 -0800

    Updated main for the GUI's qml file.

--- gui/main.py
+++ gui/main.py
@@ -9,7 +9,7 @@ from PySide2.QtQml import QQmlApplicationEngine
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    engine.load(os.path.join(os.path.dirname(__file__), "main.qml"))
+    engine.load(os.path.join(os.path.dirname(__file__), "qml/main.qml"))
 
     if not engine.rootObjects():
         sys.exit(-1)
