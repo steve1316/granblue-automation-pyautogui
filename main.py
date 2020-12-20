6e13079edf8e64ec1c9ca2819ea5d66e6cf20370
Steve Tu <stevetu11@yahoo.com>
2020-12-19 17:40:15 -0800

    Changed debug flag to be a global variable.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from game import Game
 from debug import Debug
 
+DEBUG = False
+
 
 def main():
-    my_game = Game(custom_mouse_speed=0.3, debug_mode=True)
+    my_game = Game(custom_mouse_speed=0.3, debug_mode=DEBUG)
     my_debug = Debug(my_game)
 
     # Test finding all summon element tabs in Summon Selection Screen.
