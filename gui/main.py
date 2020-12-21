79eade8e09b9b938f840e9a756e6b130116e7d8b
Steve Tu <stevetu11@yahoo.com>
2020-12-20 21:47:58 -0800

    Moved all GUI logic to the main.py in root.

--- gui/main.py
+++ gui/main.py
@@ -1,16 +1,36 @@
-# This Python file uses the following encoding: utf-8
-import sys
-import os
+# # This Python file uses the following encoding: utf-8
+# import sys
+# import os
 
-from PySide2.QtGui import QGuiApplication
-from PySide2.QtQml import QQmlApplicationEngine
+# from PySide2.QtGui import QGuiApplication
+# from PySide2.QtQml import QQmlApplicationEngine
+# from PySide2.QtCore import QObject, Slot, Signal
 
 
-if __name__ == "__main__":
-    app = QGuiApplication(sys.argv)
-    engine = QQmlApplicationEngine()
-    engine.load(os.path.join(os.path.dirname(__file__), "qml/main.qml"))
+# class MainWindow(QObject):
+#     def __init__(self):
+#         QObject.__init__(self)
 
-    if not engine.rootObjects():
-        sys.exit(-1)
-    sys.exit(app.exec_())
+#     # Signal variable to the log.
+#     updateConsoleLog = Signal(str)
+
+#     # Function set the log to the scrolling view's text.
+#     @Slot(str)
+#     def update_console_log(self, line):
+#         self.updateConsoleLog.emit("Line: " + line)
+
+
+# if __name__ == "__main__":
+#     app = QGuiApplication(sys.argv)
+#     engine = QQmlApplicationEngine()
+
+#     # Get the Context.
+#     main = MainWindow()
+#     engine.rootContext().setContextProperty("backend", main)
+
+#     # Load the QML File.
+#     engine.load(os.path.join(os.path.dirname(__file__), "qml/main.qml"))
+
+#     if not engine.rootObjects():
+#         sys.exit(-1)
+#     sys.exit(app.exec_())
--- main.py
+++ main.py
@@ -1,19 +1,59 @@
+import sys
+import os
+
+from PySide2.QtGui import QGuiApplication
+from PySide2.QtQml import QQmlApplicationEngine
+from PySide2.QtCore import QObject, Slot, Signal
+
 from game import Game
 from debug import Debug
 
 DEBUG = False
 
 
-def main():
-    my_game = Game(custom_mouse_speed=0.3, debug_mode=DEBUG)
-    my_debug = Debug(my_game)
+class MainWindow(QObject):
+    def __init__(self):
+        QObject.__init__(self)
 
-    # Test finding all summon element tabs in Summon Selection Screen.
-    # my_debug.test_find_summon_element_tabs()
+        self.myLine = "HELLO"
 
-    # Test Combat Mode.
-    # my_debug.test_combat_mode()
+    # Signal connection to the log.
+    updateConsoleLog = Signal(str)
+
+    # Function to interact with the scrolling view's text log.
+    @Slot(str)
+    def update_console_log(self, line):
+        self.updateConsoleLog.emit("\nLine: " + self.myLine)
+
+    # Start the bot.
+    def start_bot(self):
+        self.mygame = Game(custom_mouse_speed=0.3, debug_mode=DEBUG)
+        self.mydebug = Debug(self.mygame)
+
+# def main():
+#     my_game = Game(custom_mouse_speed=0.3, debug_mode=DEBUG)
+#     my_debug = Debug(my_game)
+
+#     # Test finding all summon element tabs in Summon Selection Screen.
+#     my_debug.test_find_summon_element_tabs()
+
+#     # Test Combat Mode.
+#     # my_debug.test_combat_mode()
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+
+    # Get the Context.
+    main = MainWindow()
+    engine.rootContext().setContextProperty("backend", main)
+
+    # Load the QML File.
+    engine.load(os.path.join(os.path.dirname(__file__), "gui/qml/main.qml"))
+
+    if not engine.rootObjects():
+        sys.exit(-1)
+    sys.exit(app.exec_())
