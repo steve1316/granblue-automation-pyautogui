56931f586ae53511d31f93a8f74d1e04a3d5a5cb
Steve Tu <stevetu11@yahoo.com>
2021-01-02 01:15:59 -0800

    Will now keep track of changes from the frontend for the Game class.

--- main.py
+++ main.py
@@ -18,10 +18,12 @@ class Tester:
         self.game = None
         self.debug = None
         
-    def run_bot(self, queue, isBotRunning, combat_script, debug_mode):
+    def run_bot(self, item_name, item_amount, island_name, mission_name, summon_element_name, summon_name, group_number, party_number, queue, isBotRunning, combat_script, debug_mode):
         self.game = Game(queue=queue, isBotRunning=isBotRunning, combat_script=combat_script, custom_mouse_speed=0.25, debug_mode=debug_mode)
         self.map_selection = MapSelection(self.game)
         self.debug = Debug(self.game, isBotRunning=isBotRunning, combat_script=combat_script)
+        
+        self.game.start_farming_mode(summon_element_name, summon_name, group_number, party_number, "quest", island_name, item_name, item_amount, mission_name)
         
         # Test finding amounts of all items on the screen.
         # self.debug.test_item_detection(4)
@@ -36,7 +38,7 @@ class Tester:
         # self.debug.test_find_summon_element_tabs()
 
         # Test Combat Mode.
-        self.debug.test_combat_mode()
+        # self.debug.test_combat_mode()
         # self.debug.test_combat_mode2()
         
         isBotRunning.value = 1
@@ -45,18 +47,29 @@ class MainWindow(QObject):
     def __init__(self):
         QObject.__init__(self)
         
-        # Create the Queue for storing logging messages and the flag for the bot ready/running status.
+        # Create the Queue for storing logging messages and the flag for the bot's running status.
         self.queue = multiprocessing.Queue()
-        self.isBotReady = None
         self.isBotRunning = None
         
         # Create a list in memory to hold all messages in case the frontend wants to save all those messages into a text file.
         self.text_log = []
         
+        # Hold the file path to the combat script for use during Combat Mode.
         self.real_file_path = None
                 
+        # Prep the following objects for multi-processed threading.        
         self.bot_object = Tester()
         self.bot_process = None
+        
+        # Hold the following information for the Game class initialization in a new thread.
+        self.item_name = ""
+        self.item_amount = ""
+        self.island_name = ""
+        self.mission_name = ""
+        self.summon_element_name = ""
+        self.summon_name = ""
+        self.group_number = ""
+        self.party_number = ""
         
         self.debug_mode = False
 
@@ -67,6 +80,61 @@ class MainWindow(QObject):
     checkBotStatus = Signal(bool)
     checkBotReady = Signal(bool)
     openFile = Signal(str)
+    
+    updateMessage = Signal(str)
+    
+    enableGroupAndPartySelectors = Signal()
+    
+    
+    # The following functions updates their respective variables to prep for Game class initialization.
+    @Slot(str)
+    def update_item_name(self, item_name):
+        self.item_name = item_name
+        self.updateMessage.emit("Item: " + self.item_name + "\nIsland: " + self.island_name + "\nMission: " + self.mission_name + 
+                                "\nItem amount: " + str(self.item_amount) + "\nSummon: " + self.summon_name + "\nGroup Number: " + 
+                                str(self.group_number) + "\nParty Number: " + str(self.party_number))
+        
+    @Slot(str)
+    def update_item_amount(self, item_amount):
+        self.item_amount = int(item_amount)
+        self.updateMessage.emit("Item: " + self.item_name + "\nIsland: " + self.island_name + "\nMission: " + self.mission_name + 
+                                "\nItem amount: " + str(self.item_amount) + "\nSummon: " + self.summon_name + "\nGroup Number: " + 
+                                str(self.group_number) + "\nParty Number: " + str(self.party_number))
+        
+    @Slot(str, str)
+    def update_mission_name(self, mission_name, island_name):
+        self.mission_name = mission_name
+        self.island_name = island_name
+        self.updateMessage.emit("Item: " + self.item_name + "\nIsland: " + self.island_name + "\nMission: " + self.mission_name + 
+                                "\nItem amount: " + str(self.item_amount) + "\nSummon: " + self.summon_name + "\nGroup Number: " + 
+                                str(self.group_number) + "\nParty Number: " + str(self.party_number))
+        
+    @Slot(str, str)
+    def update_summon_name(self, summon_name, summon_element):
+        formatted_summon_name = summon_name.lower().replace(" ", "_")
+        self.summon_element_name = summon_element.lower()
+        self.summon_name = formatted_summon_name
+        self.updateMessage.emit("Item: " + self.item_name + "\nIsland: " + self.island_name + "\nMission: " + self.mission_name + 
+                                "\nItem amount: " + str(self.item_amount) + "\nSummon: " + self.summon_name + "\nGroup Number: " + 
+                                str(self.group_number) + "\nParty Number: " + str(self.party_number))
+        
+        self.enableGroupAndPartySelectors.emit()
+        
+    @Slot(str)
+    def update_group_number(self, group_number):
+        split_group_number = group_number.split(" ")[1]
+        self.group_number = int(split_group_number)
+        self.updateMessage.emit("Item: " + self.item_name + "\nIsland: " + self.island_name + "\nMission: " + self.mission_name + 
+                                "\nItem amount: " + str(self.item_amount) + "\nSummon: " + self.summon_name + "\nGroup Number: " + 
+                                str(self.group_number) + "\nParty Number: " + str(self.party_number))
+        
+    @Slot(str)
+    def update_party_number(self, party_number):
+        split_party_number = party_number.split(" ")[1]
+        self.party_number = int(split_party_number)
+        self.updateMessage.emit("Item: " + self.item_name + "\nIsland: " + self.island_name + "\nMission: " + self.mission_name + 
+                                "\nItem amount: " + str(self.item_amount) + "\nSummon: " + self.summon_name + "\nGroup Number: " + 
+                                str(self.group_number) + "\nParty Number: " + str(self.party_number))
     
     # Save the text_log to the specified text file.
     @Slot(str)
@@ -101,13 +169,10 @@ class MainWindow(QObject):
         else:
             self.checkBotStatus.emit(False)
     
-    # Update the flag of the readyness of the bot.
-    @Slot(str)
-    def check_bot_ready(self, display_text):
-        if(display_text == "Please select a mission."):
-            self.checkBotReady.emit(False)
-        else:
-            self.checkBotReady.emit(True)
+    # Update the flag on whether or not the bot is ready to start.
+    @Slot(bool)
+    def check_bot_ready(self, ready_flag):
+        self.checkBotReady.emit(ready_flag)
 
     # Grab logging messages from the Queue and then output to the frontend's log.
     @Slot()
@@ -122,7 +187,9 @@ class MainWindow(QObject):
     def start_bot(self):
         print("\nStarting bot.")
         self.isBotRunning = multiprocessing.Value("i", 0)
-        self.bot_process = multiprocessing.Process(target=self.bot_object.run_bot, args=(self.queue, self.isBotRunning, self.real_file_path, self.debug_mode,))
+        self.bot_process = multiprocessing.Process(target=self.bot_object.run_bot, args=(self.item_name, self.item_amount, self.island_name, self.mission_name, 
+                                                                                         self.summon_element_name, self.summon_name, self.group_number, self.party_number , 
+                                                                                         self.queue, self.isBotRunning, self.real_file_path, self.debug_mode,))
         self.bot_process.start()
     
     # Stop the bot.
