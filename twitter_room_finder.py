6dfd7c5ccffb5f873a17b0d9b496c6984a69f210
Steve Tu <stevetu11@yahoo.com>
2021-01-14 23:48:55 -0800

    Bot can now parse room codes from JP and EN tweets.

--- twitter_room_finder.py
+++ twitter_room_finder.py
@@ -37,6 +37,14 @@ class TwitterRoomFinder():
         self.request_limit = 900
         self.rate_limit = 900 # A maximum of 900 requests per 15 minutes or 900 seconds before getting rate-limited.
         
+        self.already_visited = []
+        self.list_of_id = []
+        self.list_of_raids_jp = {
+            "Lvl 100 Odin": "Lv100 オーディン",
+            "Lvl 100 Medusa": "Lv100 メドゥーサ",
+            "Lvl 120 Grimnir": "Lv120 グリームニル",
+        }
+        
         self.debug_mode = debug_mode
         
         # Connect to Twitter's API.
@@ -44,10 +52,10 @@ class TwitterRoomFinder():
         auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
         auth.set_access_token(self.access_token, self.access_token_secret)
         self.api = tweepy.API(auth)
-        self.game.print_and_save(f"\n{self.game.printtime()} [TWITTER] Successfully connected to the Twitter API.")
+        self.game.print_and_save(f"{self.game.printtime()} [TWITTER] Successfully connected to the Twitter API.")
     
     def find_most_recent(self, raid_name: str, count: int = 10, tweets_only: bool = False):
-        """Start listening to tweets containing ':Battle ID'.
+        """Start listening to tweets containing room codes starting with JP and then listens for EN tweets if there was not enough collected tweets.
 
         Args:
             raid_name (str): Name and level of the raid that appears in tweets containing the room code to it.
@@ -57,16 +65,37 @@ class TwitterRoomFinder():
         Returns:
             tweets (Iterable[str]): List of 10 most recent tweets that match the query.
         """
-        self.game.print_and_save(f"\n{self.game.printtime()} [TWITTER] Now finding the 10 most recent tweets for {raid_name}...")
+        self.game.print_and_save(f"\n{self.game.printtime()} [TWITTER] Now finding the {count} most recent tweets for {raid_name}.")
         today = datetime.datetime.today()
-        query = f"+:Battle ID AND +{raid_name}"
+        query_en = f"+(:Battle ID) AND +({raid_name})"
+        query_jp = f"+(:参戦ID) AND +({self.list_of_raids_jp[raid_name]})"
         
         # Example of expected tweet:
         #   CUSTOM_USER_MESSAGE XXXXXXXX :Battle ID
         #   I need backup!
         #   LEVEL and NAME OF RAID
         
-        tweets = self.api.search(q=query, since=today.strftime('%Y-%m-%d'), count=count)
+        tweets = []
+        list_of_id = []
+
+        if(len(self.list_of_id) > 50):
+            self.list_of_id = []
+            
+        tweet_jp = self.api.search(q=query_jp, since=today.strftime('%Y-%m-%d'), count=count)
+        for tweet in tweet_jp:
+            if(tweet.id not in self.list_of_id and len(tweets) < count):
+                self.game.print_and_save(f"{self.game.printtime()} [TWITTER] Found JP tweet.")
+                tweets.append(tweet)
+                self.list_of_id.append(tweet.id)
+
+        if(len(tweets) < count):
+            tweet_en = self.api.search(q=query_en, since=today.strftime('%Y-%m-%d'), count=count)
+            for tweet in tweet_en:
+                if(tweet.id not in self.list_of_id and len(tweets) < count):
+                    self.game.print_and_save(f"{self.game.printtime()} [TWITTER] Found EN tweet.")
+                    tweets.append(tweet)
+                    self.list_of_id.append(tweet.id)
+                    
         return tweets
         
     def clean_tweets(self, tweets: Iterable[str]):
@@ -78,14 +107,24 @@ class TwitterRoomFinder():
         Returns:
             room_codes (Iterable[str]): List of room codes cleaned of all other text.
         """
-        self.game.print_and_save(f"{self.game.printtime()} [TWITTER] Now cleaning up and parsing for room codes...")
+        self.game.print_and_save(f"\n{self.game.printtime()} [TWITTER] Now cleaning up the tweets and parsing for room codes...\n")
         room_codes = []
         
         # Split the text up by whitespaces and find the element in the list that has the room code.
         for tweet in tweets:
+            if(len(self.already_visited) > 50):
+                self.already_visited = []
+            
             split_text = tweet.text.split(" ")
-            for i, element in enumerate(split_text):
-                if(element == ":Battle"):
-                    room_codes.append(split_text[i - 1])
+            for i, identifier in enumerate(split_text):
+                if((":Battle" in identifier) or (":参戦ID" in identifier)):
+                    parsed_code = split_text[i - 1]
+                    if(parsed_code not in self.already_visited):
+                        self.game.print_and_save(f"{self.game.printtime()} [TWITTER] Found {parsed_code} created at {tweet.created_at}")
+                        room_codes.append(parsed_code)
+                        self.already_visited.append(parsed_code)
+                        break
+                    else:
+                        self.game.print_and_save(f"{self.game.printtime()} [TWITTER] Already visited {parsed_code} before in this session. Skipping this code...")
 
         return room_codes
