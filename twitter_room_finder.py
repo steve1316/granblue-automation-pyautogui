168724dd325035b5540a2bfed42d5648e4f1cff5
Steve Tu <stevetu11@yahoo.com>
2021-01-15 13:27:32 -0800

    Added the EN names of all raids and their JP equivalents.

--- twitter_room_finder.py
+++ twitter_room_finder.py
@@ -40,9 +40,98 @@ class TwitterRoomFinder():
         self.already_visited = []
         self.list_of_id = []
         self.list_of_raids_jp = {
-            "Lvl 100 Odin": "Lv100 オーディン",
+            # Omega Raids
+            "Lvl 50 Tiamat Omega": "Lv50 ティアマト・マグナ",
+            "Lvl 100 Tiamat Omega Ayr": "Lv100 ティアマト・マグナ＝エア",
+            "Lvl 70 Colossus Omega": "Lv70 コロッサス・マグナ",
+            "Lvl 100 Colossus Omega": "Lv100 コロッサス・マグナ",
+            "Lvl 60 Leviathan Omega": "Lv60 リヴァイアサン・マグナ",
+            "Lvl 100 Leviathan Omega": "Lv100 リヴァイアサン・マグナ",
+            "Lvl 60 Yggdrasil Omega": "Lv60 ユグドラシル・マグナ",
+            "Lvl 100 Yggdrasil Omega": "Lv100 ユグドラシル・マグナ",
+            "Lvl 75 Luminiera Omega": "Lv75 シュヴァリエ・マグナ",
+            "Lvl 100 Luminiera Omega": "Lv100 シュヴァリエ・マグナ",
+            "Lvl 75 Celeste Omega": "Lv75 セレスト・マグナ",
+            "Lvl 100 Celeste Omega": "Lv100 セレスト・マグナ",
+            
+            # Tier 1 Summon Raids
+            "Lvl 100 Twin Elements": "Lv100 フラム＝グラス",
+            "Lvl 120 Twin Elements": "Lv120 フラム＝グラス",
+            "Lvl 100 Macula Marius": "Lv100 マキュラ・マリウス",
+            "Lvl 120 Macula Marius": "Lv120 マキュラ・マリウス",
             "Lvl 100 Medusa": "Lv100 メドゥーサ",
+            "Lvl 120 Medusa": "Lv120 メドゥーサ",
+            "Lvl 100 Nezha": "Lv100 ナタク",
+            "Lvl 120 Nezha": "Lv120 ナタク",
+            "Lvl 100 Apollo": "Lv100 アポロン",
+            "Lvl 120 Apollo": "Lv120 アポロン",
+            "Lvl 100 Dark Angel Olivia": "Lv100 Dエンジェル・オリヴィエ",
+            "Lvl 120 Dark Angel Olivia": "Lv120 Dエンジェル・オリヴィエ",
+            
+            # Tier 2 Summon Raids
+            "Lvl 100 Athena": "Lv100 アテナ",
+            "Lvl 100 Grani": "Lv100 グラニ",
+            "Lvl 100 Baal": "Lv100 バアル",
+            "Lvl 100 Garuda": "Lv100 ガルーダ",
+            "Lvl 100 Odin": "Lv100 オーディン",
+            "Lvl 100 Lich": "Lv100 リッチ",
+            
+            # Primarch Raids
+            "Lvl 100 Michael": "Lv100 ミカエル",
+            "Lvl 100 Gabriel": "Lv100 ガブリエル",
+            "Lvl 100 Uriel": "Lv100 ウリエル",
+            "Lvl 100 Raphael": "Lv100 ラファエル",
+            "The Four Primarchs": "四大天司ＨＬ",
+            
+            # Nightmare Raids
+            "Lvl 100 Proto Bahamut": "Lv100 プロトバハムート",
+            "Lvl 150 Proto Bahamut": "Lv150 プロトバハムート",
+            "Lvl 150 Ultimate Bahamut": "Lv150 アルティメットバハムート",
+            "Lvl 200 Ultimate Bahamut": "Lv200 アルティメットバハムート",
+            "Lvl 100 Grand Order": "Lv100 ジ・オーダー・グランデ",
+            "Lvl 200 Grand Order": "Lv200 ジ・オーダー・グランデ",
+            
+            # Rise of the Beasts Raids
+            "Lvl 60 Zhuque": "Lv60 朱雀",
+            "Lvl 90 Agni": "Lv90 アグニス",
+            "Lvl 60 Xuanwu": "Lv60 玄武",
+            "Lvl 90 Neptune": "Lv90 ネプチューン",
+            "Lvl 60 Baihu": "Lv60 白虎",
+            "Lvl 90 Titan": "Lv90 ティターン",
+            "Lvl 60 Qinglong": "Lv60 青竜",
+            "Lvl 90 Zephyrus": "Lv90 ゼピュロス",
+            "Lvl 100 Huanglong": "Lv100 黄龍",
+            "Lvl 100 Qilin": "Lv100 黒麒麟",
+            "Huanglong & Qilin (Impossible)": "黄龍・黒麒麟HL",
+            "Lvl 100 Shenxian": "Lv100 四象瑞神",
+
+            # Impossible Raids
+            "Lvl 110 Rose Queen": "Lv110 ローズクイーン",
+            "Lvl 120 Shiva": "Lv120 シヴァ",
+            "Lvl 120 Europa": "Lv120 エウロペ",
+            "Lvl 120 Godsworn Alexiel": "Lv120 ゴッドガード・ブローディア",
             "Lvl 120 Grimnir": "Lv120 グリームニル",
+            "Lvl 120 Metatron": "Lv120 メタトロン",
+            "Lvl 120 Avatar": "Lv120 アバター",
+            "Lvl 120 Prometheus": "Lv120 プロメテウス",
+            "Lvl 120 Ca Ong": "Lv120 カー・オン",
+            "Lvl 120 Gilgamesh": "Lv120 ギルガメッシュ",
+            "Lvl 120 Morrigna": "Lv120 バイヴカハ",
+            "Lvl 120 Hector": "Lv120 ヘクトル",
+            "Lvl 120 Anubis": "Lv120 アヌビス",
+            "Lvl 150 Tiamat Malice": "Lv150 ティアマト・マリス",
+            "Lvl 150 Leviathan Malice": "Lv150 リヴァイアサン・マリス",
+            "Lvl 150 Phronesis": "Lv150 フロネシス",
+            "Lvl 200 Akasha": "Lv200 アーカーシャ",
+            "Lvl 150 Lucilius": "Lv150 ルシファー",
+            "Lvl 250 Lucilius": "Lv250 ルシファー",
+            "Lvl 200 Wilnas": "Lv200 ウィルナス",
+            "Lvl 200 Wamdus": "Lv200 ワムデュス",
+            "Lvl 200 Galleon": "Lv200 ガレヲン",
+            "Lvl 200 Ewiyar": "Lv200 イーウィヤ",
+            "Lvl 200 Lu Woh": "Lv200 ル・オー",
+            "Lvl 200 Fediel": "Lv200 フェディエル",
+            "Lvl 250 Beelzebub": "Lv250 ベルゼバブ"
         }
         
         self.debug_mode = debug_mode
