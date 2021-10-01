99fff35c08974781a16601e295963595ea92e75d
Steve Tu <stevetu11@yahoo.com>
2021-10-01 00:41:50 -0700

    Initial mockup for the Home component

--- gui/src/pages/Home/index.tsx
+++ gui/src/pages/Home/index.tsx
@@ -1,13 +1,20 @@
-import React, { useEffect } from "react"
+import "./index.scss"
 
 const Home = () => {
-    useEffect(() => {
-        document.title = "Granblue Automation - Home"
-    }, [])
+    const initialMessage = `Welcome to Granblue Automation! 
+    \n*************************** 
+    \nInstructions\n----------------
+    \nNote: The START button is disabled until the following steps are followed through.
+    \n1. Please have your game window fully visible.
+    \n2. Go into the Settings Page and follow the on-screen messages to guide you through setting up the bot.
+    \n3. You can head back to the Home Page and click START.
+    \n\n***************************`
 
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="logOuterContainer">
+            <div className="logInnerContainer">
+                <p id="log">{initialMessage}</p>
+            </div>
         </div>
     )
 }
