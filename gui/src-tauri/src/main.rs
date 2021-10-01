98f9906f1daeda1070ffb1268595754f52179b3c
Steve Tu <stevetu11@yahoo.com>
2021-10-01 00:39:59 -0700

    Updated .exe name and added a splashscreen when starting up

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -3,8 +3,27 @@
   windows_subsystem = "windows"
 )]
 
+use std::time::Duration;
+use std::thread::sleep;
+use rand::Rng;
+use tauri::Manager;
+#[tauri::command]
+fn close_splashscreen(window: tauri::Window) {
+  // Wait a random number of seconds.
+  let mut rng = rand::thread_rng();
+  sleep(Duration::new(rng.gen_range(1..3), 0));
+
+  // Close splashscreen window.
+  if let Some(splashscreen) = window.get_window("splashscreen") {
+    splashscreen.close().unwrap();
+  }
+  // Show main window.
+  window.get_window("main").unwrap().show().unwrap();
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![close_splashscreen])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- gui/src/index.js
+++ gui/src/index.js
@@ -2,6 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
+import { invoke } from "@tauri-apps/api/tauri"
+
+// This will wait for the window to load before closing the splashscreen using the following rust command.
+document.addEventListener("DOMContentLoaded", () => {
+    invoke("close_splashscreen")
+})
 
 ReactDOM.render(
     <React.StrictMode>
