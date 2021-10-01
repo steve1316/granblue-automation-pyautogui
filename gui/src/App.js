7c11d2fc26222044514074917387e9d99d731df3
Steve Tu <stevetu11@yahoo.com>
2021-10-01 00:40:42 -0700

    Added a navbar component

--- gui/src/App.js
+++ gui/src/App.js
@@ -1,15 +1,19 @@
 import Home from "./pages/Home"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import NavBar from "./components/NavBar"
 
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/" component={Home} exact>
-                    <Home />
-                </Route>
-            </Switch>
-        </Router>
+        <main>
+            <Router>
+                <NavBar />
+                <Switch>
+                    <Route path="/" component={Home} exact>
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
+        </main>
     )
 }
 
---
+++ gui/src/components/NavBar/index.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react"
+import { AppBar, Button, ButtonGroup, Divider, Drawer, IconButton, List, ListItem, ListItemText, Toolbar, Typography } from "@mui/material"
+import { Close, CropSquare, Menu, Minimize } from "@mui/icons-material"
+import { Link as RouterLink, useHistory } from "react-router-dom"
+import "./index.scss"
+import { appWindow } from "@tauri-apps/api/window"
+
+const NavBar = () => {
+    const history = useHistory()
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+
+    const toggleDrawer = () => {
+        setIsDrawerOpen(!isDrawerOpen)
+    }
+
+    return (
+        <AppBar position="static" id="header">
+            <Toolbar variant="dense" className="body" data-tauri-drag-region>
+                <IconButton className="menuButton" size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={toggleDrawer}>
+                    <Menu />
+                </IconButton>
+                <Drawer anchor="left" open={isDrawerOpen} onClose={toggleDrawer}>
+                    <List>
+                        <RouterLink to="/" className="link">
+                            <ListItem button key="home">
+                                <ListItemText primary="Home" />
+                            </ListItem>
+                            <Divider />
+                        </RouterLink>
+                    </List>
+                </Drawer>
+                <Typography
+                    variant="h6"
+                    className="title"
+                    onClick={() => {
+                        history.push("/")
+                    }}
+                >
+                    Granblue Automation
+                </Typography>
+                <div className="emptyDivider" />
+                <ButtonGroup variant="outlined">
+                    <Button className="navButton" onClick={() => appWindow.minimize()}>
+                        <Minimize />
+                    </Button>
+                    <Button className="navButton" onClick={() => appWindow.toggleMaximize()}>
+                        <CropSquare />
+                    </Button>
+                    <Button className="navButton" onClick={() => appWindow.close()}>
+                        <Close />
+                    </Button>
+                </ButtonGroup>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default NavBar
