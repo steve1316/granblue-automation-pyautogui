ac64bd0d9def2580b3454222a230517057bbdab6
Steve Tu <stevetu11@yahoo.com>
2021-10-02 12:04:13 -0700

    Removed main tag

--- gui/src/App.js
+++ gui/src/App.js
@@ -5,19 +5,17 @@ import Settings from "./pages/Settings"
 
 function App() {
     return (
-        <main>
-            <Router>
-                <NavBar />
-                <Switch>
-                    <Route path="/" component={Home} exact>
-                        <Home />
-                    </Route>
-                    <Route path="/settings" component={Settings} exact>
-                        <Settings />
-                    </Route>
-                </Switch>
-            </Router>
-        </main>
+        <Router>
+            <NavBar />
+            <Switch>
+                <Route path="/" component={Home} exact>
+                    <Home />
+                </Route>
+                <Route path="/settings" component={Settings} exact>
+                    <Settings />
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
