ea477e74cd46e266fc077282cf2e1961da488527
Steve Tu <stevetu11@yahoo.com>
2021-10-21 16:43:45 -0700

    Fleshed out Home page and added a Start button

--- gui/src/pages/Home/index.tsx
+++ gui/src/pages/Home/index.tsx
@@ -1,23 +1,38 @@
-import { Fade } from "@mui/material"
+import { useContext } from "react"
+import { Button, Fade, Stack } from "@mui/material"
+import { Box } from "@mui/system"
 import "./index.scss"
+import { ReadyContext } from "../../context/ReadyContext"
 
 const Home = () => {
-    const initialMessage = `Welcome to Granblue Automation! 
-    \n*************************** 
-    \nInstructions\n----------------
+    const { status } = useContext(ReadyContext)
+
+    const initialMessage = `Welcome to Granblue Automation!
+    \n***************************\n\nInstructions\n----------------
     \nNote: The START button is disabled until the following steps are followed through.
-    \n1. Please have your game window fully visible.
-    \n2. Go into the Settings Page and follow the on-screen messages to guide you through setting up the bot.
-    \n3. You can head back to the Home Page and click START.
-    \n\n***************************`
+    \n1. Have your game window and the Bottom Menu visible. Set the game window size set to the second "notch". 
+    \n2. Go to the Settings Page of the bot and fill out the sections until the status at the top says "Ready".
+    \n3. You can now head back to the Home Page of the bot and click START.
+    \n***************************`
 
     return (
         <Fade in={true}>
-            <div className="logOuterContainer">
-                <div className="logInnerContainer">
-                    <p id="log">{initialMessage}</p>
-                </div>
-            </div>
+            <Box className="homeContainer" id="homeContainer">
+                <Stack direction="row" sx={{ height: "100%" }}>
+                    <div className="logOuterContainer">
+                        <div className="logInnerContainer">
+                            <p id="log">{initialMessage}</p>
+                        </div>
+                    </div>
+                    <div className="rightOuterContainer">
+                        <div className="rightContainer">
+                            <Button disabled={!status} variant="contained">
+                                {status ? "Start" : "Not Ready"}
+                            </Button>
+                        </div>
+                    </div>
+                </Stack>
+            </Box>
         </Fade>
     )
 }
