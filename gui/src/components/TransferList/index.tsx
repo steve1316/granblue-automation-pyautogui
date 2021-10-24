b0ccb7ed4ff4175a8b7a7c5e4bc35b2f5255256e
Steve Tu <stevetu11@yahoo.com>
2021-10-24 14:19:55 -0700

    Support Summon selector is now populated

--- gui/src/components/TransferList/index.tsx
+++ gui/src/components/TransferList/index.tsx
@@ -1,12 +1,41 @@
-import { useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Card, CardActionArea, CardMedia, Divider, Grid, Grow, List, ListItem, ListItemText, Paper, Tooltip, Typography, Zoom } from "@mui/material"
 import "./index.scss"
 
-const TransferList = () => {
-    const [leftList, setLeftList] = useState<number[]>([1, 2, 31, 2, 31, 2, 3])
-    const [rightList, setRightList] = useState<number[]>([4])
+import summonData from "../../data/summons.json"
+import { BotStateContext } from "../../context/BotStateContext"
 
-    const handleChecked = (value: number, isLeftList: boolean) => () => {
+const TransferList = () => {
+    const [leftList, setLeftList] = useState<string[]>([])
+    const [rightList, setRightList] = useState<string[]>([])
+
+    const botStateContext = useContext(BotStateContext)
+
+    // Populate the Support Summon List.
+    useEffect(() => {
+        // Populate the left list.
+        var oldLeftList: string[] = leftList
+
+        Object.entries(summonData).forEach((key) => {
+            key[1].summons.forEach((summon) => {
+                oldLeftList = [...oldLeftList, summon]
+            })
+        })
+
+        oldLeftList = Array.from(new Set(oldLeftList))
+
+        // Populate the right list.
+        var oldRightList: string[] = botStateContext?.summons
+
+        // Filter out summons from the left list that are already selected.
+        const filteredList = oldLeftList.filter((summon) => !oldRightList.includes(summon))
+
+        setLeftList(filteredList)
+        setRightList(oldRightList)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    const handleChecked = (value: string, isLeftList: boolean) => () => {
         if (isLeftList) {
             // Handle the left list.
             const index = leftList.indexOf(value)
@@ -17,6 +46,9 @@ const TransferList = () => {
             // Move the element to the right list.
             const newRightList = [...rightList, value]
             setRightList(newRightList)
+
+            // Save selected summons to settings.
+            botStateContext?.setSummons(newRightList)
         } else {
             // Handle the right list
             const index = rightList.indexOf(value)
@@ -27,10 +59,13 @@ const TransferList = () => {
             // Move the element to the left list.
             const newLeftList = [...leftList, value]
             setLeftList(newLeftList)
+
+            // Save selected summons to settings.
+            botStateContext?.setSummons(newRightList)
         }
     }
 
-    const customList = (items: number[], isLeftList: boolean) => (
+    const customList = (items: string[], isLeftList: boolean) => (
         <Card className="card">
             <Paper elevation={2}>
                 <Typography variant="h5" component="div" className="cardHeader">
@@ -48,15 +83,15 @@ const TransferList = () => {
                     overflow: "auto",
                 }}
             >
-                {items.map((value: number) => {
-                    const name = `test-${value}`
+                {items.map((value: string, index: number) => {
+                    const fileName = value.replaceAll(" ", "_").toLowerCase()
                     return (
-                        <Tooltip title={name} placement="left" arrow TransitionComponent={Zoom} TransitionProps={{ timeout: 200 }}>
+                        <Tooltip key={`${index}-${value}`} title={`${value}`} placement="left" arrow TransitionComponent={Zoom} TransitionProps={{ timeout: 200 }}>
                             <Grow in={true}>
-                                <ListItem key={value} button onClick={handleChecked(value, isLeftList)}>
+                                <ListItem button onClick={handleChecked(value, isLeftList)}>
                                     <Card className="supportSummonCard">
                                         <CardActionArea>
-                                            <CardMedia component="img" image={require("../../images/agni.png").default} />
+                                            <CardMedia component="img" image={require(`../../images/summons/${fileName}.png`).default} />
                                         </CardActionArea>
                                     </Card>
                                     <ListItemText className="listItemText" primary={value} />
