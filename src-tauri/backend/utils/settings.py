fa8351d7f4bac83421ae13d2692a12832c8eab3b
Steve Tu <stevetu11@yahoo.com>
2021-11-13 14:36:16 -0800

    Fixed bug where Nightmare settings were not accessed correctly

--- src-tauri/backend/utils/settings.py
+++ src-tauri/backend/utils/settings.py
@@ -67,33 +67,33 @@ class Settings:
     enable_nightmare: bool = _data["nightmare"]["enableNightmare"]
     _enable_custom_nightmare_settings: bool = _data["nightmare"]["enableCustomNightmareSettings"]
     _farming_modes_with_nightmares = ["Event", "Event (Token Drawboxes)", "Rise of the Beasts", "Xeno Clash"]
+    nightmare_combat_script: List[str] = _data["nightmare"]["nightmareCombatScript"]
+    nightmare_combat_script_name: str = _data["nightmare"]["nightmareCombatScriptName"]
+    nightmare_summon_list: List[str] = _data["nightmare"]["nightmareSummons"]
+    nightmare_summon_elements_list: List[str] = _data["nightmare"]["nightmareSummonElements"]
+    nightmare_group_number: int = _data["nightmare"]["nightmareGroupNumber"]
+    nightmare_party_number: int = _data["nightmare"]["nightmarePartyNumber"]
     if enable_nightmare and ((farming_mode == "Special" and mission_name == "VH Angel Halo") or _farming_modes_with_nightmares.__contains__(mission_name)):
         MessageLog.print_message(f"\n[NIGHTMARE] Initializing settings for {farming_mode}'s Nightmare...")
 
         if _enable_custom_nightmare_settings:
             # Start checking for validity and if not, default back to the settings for Farming Mode.
-            nightmare_combat_script: List[str] = _data["nightmare"]["nightmareCombatScript"]
-            nightmare_combat_script_name: str = _data["nightmare"]["nightmareCombatScriptName"]
             if len(nightmare_combat_script) == 0:
                 MessageLog.print_message(f"[NIGHTMARE] Combat Script for {farming_mode}'s Nightmare will reuse the one for Farming Mode.")
                 nightmare_combat_script = combat_script
 
-            nightmare_summon_list: List[str] = _data["nightmare"]["nightmareSummons"]
             if len(nightmare_summon_list) == 0:
                 MessageLog.print_message(f"[NIGHTMARE] Summons for {farming_mode}'s Nightmare will reuse the ones for Farming Mode.")
                 nightmare_summon_list = summon_list
 
-            nightmare_summon_elements_list: List[str] = _data["nightmare"]["nightmareSummonElements"]
             if len(nightmare_summon_elements_list) == 0:
                 MessageLog.print_message(f"[NIGHTMARE] Summon Elements for {farming_mode}'s Nightmare will reuse the ones for Farming Mode.")
                 nightmare_summon_elements_list = summon_element_list
 
-            nightmare_group_number: int = _data["nightmare"]["nightmareGroupNumber"]
             if nightmare_group_number < 1 or nightmare_group_number > 7:
                 MessageLog.print_message(f"[NIGHTMARE] Group Number for {farming_mode}'s Nightmare will reuse the one for Farming Mode.")
                 nightmare_group_number = group_number
 
-            nightmare_party_number: int = _data["nightmare"]["nightmarePartyNumber"]
             if nightmare_party_number < 1 or nightmare_party_number > 6:
                 MessageLog.print_message(f"[NIGHTMARE] Party Number for {farming_mode}'s Nightmare will reuse the one for Farming Mode.")
                 nightmare_party_number = party_number
