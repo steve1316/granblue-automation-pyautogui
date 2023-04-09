133d996314e928941c3270086b28a2ac4a6b1c0e
Steve Tu <stevetu11@yahoo.com>
2023-04-08 19:48:39 -0700

    Fix scrolling into view when more log messages are added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,22 +24,20 @@ const Home = () => {
     // Scroll to the bottom of the message log window when new messages are added.
     useEffect(() => {
         if (msgRef) {
-            const bottom: number = msgRef.current!!.scrollHeight - msgRef.current!!.clientHeight
-            msgRef.current!!.scrollTo(0, bottom)
+            msgRef.current!!.scrollIntoView({ behavior: "smooth" })
         }
     }, [mlc.messageLog])
 
     const initialMessage = `****************************************\nWelcome to Granblue Automation!\n****************************************\nInstructions\n----------------\nNote: The START button is disabled until the following steps are followed through.\n
-    1. Have your game window and the Bottom Menu visible. Set the game window size set to the second "notch". 
+    1. Have your game window and the Bottom Menu visible. Set the game window size set to the second "notch".
     2. Go to the Settings Page of the bot and fill out the sections until the status at the top says "Ready".
     3. You can now head back to the Home Page of the bot and click START.
     \nWarning: Do not refresh/F5 the program's "page" while the bot process is running. Otherwise in order to stop it, you will need to kill it by completely exiting the program.\n****************************************\n`
 
     return (
         <Container className={classes.container}>
-            <Text id="log" ref={msgRef}>
-                {initialMessage + mlc.messageLog.join("\r")}
-            </Text>
+            <Text id="log">{initialMessage + mlc.messageLog.join("\r")}</Text>
+            <div ref={msgRef} />
         </Container>
     )
 }
