6233fa50cb6b31c501709a78f6593ac2ec66d731
Steve Tu <stevetu11@yahoo.com>
2023-04-29 20:25:46 -0700

    Add list of raids with their nav positions

--- src-tauri/backend/bot/game_modes/raid.py
+++ src-tauri/backend/bot/game_modes/raid.py
@@ -17,6 +17,139 @@ class Raid:
 
     _raids_joined = 0
 
+    # List of supported raids in the format of "X,Y,Z":
+    # X = 1 for Standard tab or 2 for Impossible tab.
+    # Y = Position in the 3xN grid reading from left to right, top to bottom.
+    # Z = Position in the list reading from top to bottom.
+    _list_of_raids = {
+        ########## Standard Tab ##########
+        # 2 stars
+        "Lvl 50 Tiamat Omega": "1,2,1",
+        "Lvl 70 Colossus Omega": "1,2,2",
+        "Lvl 60 Leviathan Omega": "1,2,3",
+        "Lvl 60 Yggdrasil Omega": "1,2,4",
+        "Lvl 75 Luminiera Omega": "1,2,5",
+        "Lvl 75 Celeste Omega": "1,2,6",
+
+        # 3 stars
+        "Lvl 100 Athena": "1,3,1",
+        "Lvl 100 Grani": "1,3,2",
+        "Lvl 100 Baal": "1,3,3",
+        "Lvl 100 Garuda": "1,3,4",
+        "Lvl 100 Odin": "1,3,5",
+        "Lvl 100 Lich": "1,3,6",
+
+        # 4 stars
+        "Lvl 100 Grand Order": "1,4,1",
+        "Lvl 100 Proto Bahamut": "1,4,2",
+        "Lvl 100 Huanglong": "1,4,3",
+        "Lvl 100 Qilin": "1,4,4",
+
+        # 5 stars
+        "Lvl 100 Michael": "1,5,1",
+        "Lvl 100 Gabriel": "1,5,2",
+        "Lvl 100 Uriel": "1,5,3",
+        "Lvl 100 Raphael": "1,5,4",
+
+        # 6 stars
+        "Lvl 150 Ultimate Bahamut": "1,6,1",
+
+        ########## Impossible Tab ##########
+        # 1 stars
+        "Lvl 100 Tiamat Omega Ayr": "2,1,1",
+        "Lvl 100 Colossus Omega": "2,1,2",
+        "Lvl 100 Leviathan Omega": "2,1,3",
+        "Lvl 100 Yggdrasil Omega": "2,1,4",
+        "Lvl 100 Luminiera Omega": "2,1,5",
+        "Lvl 100 Celeste Omega": "2,1,6",
+
+        # 2 stars
+        "Lvl 120 Shiva": "2,2,1",
+        "Lvl 120 Europa": "2,2,2",
+        "Lvl 120 Godsworn Alexiel": "2,2,3",
+        "Lvl 120 Grimnir": "2,2,4",
+        "Lvl 120 Metatron": "2,2,5",
+        "Lvl 120 Avatar": "2,2,6",
+        "Lvl 110 Rose Queen": "2,2,7",
+        "Lvl 120 Atum": "2,3,1",
+        "Lvl 120 Tefnut": "2,3,2",
+        "Lvl 120 Bennu": "2,3,3",
+        "Lvl 120 Ra": "2,3,4",
+        "Lvl 120 Horus": "2,3,5",
+        "Lvl 120 Osiris": "2,3,6",
+
+        # 3 stars
+        "Lvl 150 Tiamat Malice": "2,4,1",
+        "Lvl 150 Leviathan Malice": "2,4,2",
+        "Lvl 150 Phronesis": "2,4,3",
+        "Lvl 150 Luminiera Malice": "2,4,4",
+        "Lvl 150 Anima-Animus Core": "2,4,5",
+
+        # 4 stars
+        "Huanglong & Qilin (Impossible)": "2,5,1",
+        "Lvl 150 Lucilius": "2,5,2",
+        "The Four Primarchs": "2,5,3",
+        "Lvl 200 Lindwurm": "2,5,4",
+
+        # 5 stars
+        "Lvl 200 Wilnas": "2,6,1",
+        "Lvl 200 Wamdus": "2,6,2",
+        "Lvl 200 Galleon": "2,6,3",
+        "Lvl 200 Ewiyar": "2,6,4",
+        "Lvl 200 Lu Woh": "2,6,5",
+        "Lvl 200 Fediel": "2,6,6",
+
+        # 6 stars
+        "Lvl 150 Proto Bahamut": "2,7,1",
+        "Lvl 200 Akasha": "2,7,2",
+        "Lvl 200 Grand Order": "2,7,3",
+        "Lvl 200 Ultimate Bahamut": "2,7,4",
+
+        # 7 stars
+        "Lvl 250 Lucilius": "2,8,1",
+        "Lvl 250 Belial": "2,8,2",
+        "Lvl 250 Beelzebub": "2,8,3",
+
+        # 8 stars
+        "Lvl 275 Mugen": "2,9,1",
+        "Lvl 275 Diaspora": "2,9,2",
+        "Lvl 275 Siegfried": "2,9,3",
+        "Lvl 275 Seofon": "2,9,4",
+        "Lvl 275 Agastia": "2,9,5",
+        "Lvl 300 Super Ultimate Bahamut": "2,10,1",
+
+        # TODO: These got consolidated into Quest missions.
+        "Lvl 100 Twin Elements": "Lv100 フラム＝グラス",
+        "Lvl 120 Twin Elements": "Lv120 フラム＝グラス",
+        "Lvl 100 Macula Marius": "Lv100 マキュラ・マリウス",
+        "Lvl 120 Macula Marius": "Lv120 マキュラ・マリウス",
+        "Lvl 100 Medusa": "Lv100 メドゥーサ",
+        "Lvl 120 Medusa": "Lv120 メドゥーサ",
+        "Lvl 100 Nezha": "Lv100 ナタク",
+        "Lvl 120 Nezha": "Lv120 ナタク",
+        "Lvl 100 Apollo": "Lv100 アポロン",
+        "Lvl 120 Apollo": "Lv120 アポロン",
+        "Lvl 100 Dark Angel Olivia": "Lv100 Dエンジェル・オリヴィエ",
+        "Lvl 120 Dark Angel Olivia": "Lv120 Dエンジェル・オリヴィエ",
+        "Lvl 120 Prometheus": "Lv120 プロメテウス",
+        "Lvl 120 Ca Ong": "Lv120 カー・オン",
+        "Lvl 120 Gilgamesh": "Lv120 ギルガメッシュ",
+        "Lvl 120 Morrigna": "Lv120 バイヴカハ",
+        "Lvl 120 Hector": "Lv120 ヘクトル",
+        "Lvl 120 Anubis": "Lv120 アヌビス",
+
+        # TODO: Need to see what the next ROTB will look like for the new raid finder.
+        "Lvl 60 Zhuque": "Lv60 朱雀",
+        "Lvl 90 Agni": "Lv90 アグニス",
+        "Lvl 60 Xuanwu": "Lv60 玄武",
+        "Lvl 90 Neptune": "Lv90 ネプチューン",
+        "Lvl 60 Baihu": "Lv60 白虎",
+        "Lvl 90 Titan": "Lv90 ティターン",
+        "Lvl 60 Qinglong": "Lv60 青竜",
+        "Lvl 90 Zephyrus": "Lv90 ゼピュロス",
+        "Lvl 100 Shenxian": "Lv100 四象瑞神",
+    }
+
     @staticmethod
     def _check_for_joined_raids():
         """Check and update the number of raids currently joined.
